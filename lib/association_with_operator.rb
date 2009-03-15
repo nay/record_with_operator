817562a5dc3516563903ff3f829622cc8049a354
yoshuki <yoshuki_at_saikyoline_dot_jp>
2009-03-16 03:20:10 +0900

    Override "ActiveRecord::Reflection::ThroughReflection#insert_record" for operator. ("unless condition" should be rechecked.)

--- lib/association_with_operator.rb
+++ lib/association_with_operator.rb
@@ -21,4 +21,23 @@ module AssociationWithOperator
     results
   end
 
+  def insert_record(record, *args)
+    return super unless @reflection.kind_of?(ActiveRecord::Reflection::ThroughReflection)
+
+    if record.new_record?
+      if force
+        record.save!
+      else
+        return false unless record.save
+      end
+    end
+    through_reflection = @reflection.through_reflection
+    klass = through_reflection.klass
+    association = klass.send(:with_scope, :create => construct_join_attributes(record)) { through_reflection.build_association }
+    if association.respond_to?(:operator=)
+      association.operator ||= @owner.operator
+      association.operator ||= record.operator
+    end
+    @owner.send(@reflection.through_reflection.name).proxy_target << association
+  end
 end
