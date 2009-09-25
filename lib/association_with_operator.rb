e3dccda9f129169fd6f9154f2c2047ca76cd5051
relax4u <wanderers.room@gmail.com>
2009-09-25 15:31:36 +0900

    Fixed bug "duplication find with NamedScope"

--- lib/association_with_operator.rb
+++ lib/association_with_operator.rb
@@ -13,10 +13,12 @@ module AssociationWithOperator
 
   def method_missing(method, *args)
     results = super
-    if results.respond_to?(:operator=)
-      results.operator= proxy_owner.operator
-    elsif results.kind_of? Array
-      results.each{|r| r.operator = proxy_owner.operator if r.respond_to?(:operator=)}
+    unless ActiveRecord::NamedScope::Scope === results
+      if results.respond_to?(:operator=)
+        results.operator= proxy_owner.operator
+      elsif results.kind_of? Array
+        results.each{|r| r.operator = proxy_owner.operator if r.respond_to?(:operator=)}
+      end
     end
     results
   end
