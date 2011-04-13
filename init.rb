784c07af435c6d8b215b1b3523277b441098103b
Brian Buchalter <bal711@gmail.com>
2011-04-13 11:16:18 -0400

    Moved tasks into lib dir.

--- init.rb
+++ init.rb
@@ -1,8 +1,1 @@
 ActiveRecord::Base.instance_eval{include RecordWithOperator} unless ActiveRecord::Base.include?(RecordWithOperator)
-ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
-  def operatorstamps
-    column(:created_by, :integer)
-    column(:updated_by, :integer)
-    column(:deleted_by, :integer)
-  end
-end
