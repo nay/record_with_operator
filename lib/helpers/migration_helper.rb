7bda8de80b7ee21abcbb41d3068f72ae7a839dd9
Brian Buchalter <bal711@gmail.com>
2011-04-13 13:15:50 -0400

    Fixed migration helper so it works inside a GEM.

--- lib/helpers/migration_helper.rb
+++ lib/helpers/migration_helper.rb
@@ -1,3 +1,4 @@
+require 'active_record/connection_adapters/abstract/schema_definitions'
 ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
   def operator_stamps
     column(:created_by, :integer)
