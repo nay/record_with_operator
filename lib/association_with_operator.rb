218f8ba5e69a34ad218c53ce8e4876fefee91074
nay3 <y.ohba@everyleaf.com>
2011-05-02 23:04:11 +0900

    Fixed a bug around association's dependent-destroy.

--- lib/association_with_operator.rb
+++ lib/association_with_operator.rb
@@ -13,7 +13,7 @@ module AssociationWithOperator
 
   def method_missing(method, *args)
     results = super
-    unless ActiveRecord::NamedScope::Scope === results
+    unless ActiveRecord::Relation === results
       if results.respond_to?(:operator=)
         results.operator= proxy_owner.operator
       elsif results.kind_of? Array
